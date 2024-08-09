2ac418fd1e01f90b99e1f4c20c660bca7a56c3ae
Leonid Lobanov <leonid.lobanov@pixster.eu>
2024-08-09 11:54:34 +0300

    Initial playing cards design

--- Fool.Core/obj/Debug/net8.0/Fool.Core.AssemblyInfo.cs
+++ Fool.Core/obj/Debug/net8.0/Fool.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fool.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d3275887d5604a5c7c474554e38fa19669a646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0e9e9d243f4f307bb71e7725cbc6ccfc66e8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fool.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fool.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
